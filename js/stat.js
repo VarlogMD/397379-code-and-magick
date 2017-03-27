38f43791653aefb31e828e4fe3c8b0e83b677d68
VarlogMD <demike1987@hotmail.com>
2017-03-27 23:12:26 +0300

    Добваил исправления

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,15 @@
-// stats.js
+//stats.js
 
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  // тень
+  //тень
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
-  // облако
+  //облако
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
   ctx.fillRect(100, 10, 420, 270);
-  // текст
+  //текст
   ctx.fillStyle = '#000'; 
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
@@ -17,7 +17,7 @@ window.renderStatistics = function (ctx, names, times) {
   
   var max = -1;
   var maxIndex = -1;
-  // расчет максимального времени
+  //расчет максимального времени
   for (var i = 0 ; i < times.length; i++) {  
     var time = times[i];
     if (time > max) {
@@ -25,31 +25,31 @@ window.renderStatistics = function (ctx, names, times) {
       maxIndex = i;
     }
   }
-  // ищем индекс игрока в .names[]
+  //ищем индекс игрока в .names[]
   var r = names.indexOf('Вы'); 
-  // лимит высоты гистограммы
+  //лимит высоты гистограммы
   var histogramHeight = 150;
   var step = histogramHeight / (max - 0); 
-  // рисуем гистограмму игрока 'Вы'
+  //рисуем гистограмму игрока 'Вы'
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';   
   ctx.fillRect(150, 240, 40, - times[r] * step);
   ctx.fillStyle = '#000';
   ctx.fillText(names[r], 150, 260);
   ctx.fillText(times[r], 150, 240 - times[r] * step - 10); 
-  // убераем 'Вы' из .names[]
+  //убераем 'Вы' из .names[]
   names.splice(r, 1); 
-  // генерируем прозрачность
+  //генерируем прозрачность
   function randomInteger(min, max) { 
     var rand = min + Math.random() * (max + 1 - min);
     rand = Math.floor(rand);
     return rand;
     }
-  // Рисуем гистограммы остальных участников
+  //Рисуем гистограммы остальных участников
   for(var i = 0; i < times.length -1; i++) {
     ctx.fillStyle = 'rgba(0, 0, 255,' + ((randomInteger(2, 10) * 0.1).toFixed(1)) + ')';
     ctx.fillRect(240 + 90 * i, 240, 40, -times[i] * step);
   }
-  // рисуем имена и время остальных участников
+  //рисуем имена и время остальных участников
   for(var i = 0; i < times.length -1; i++){
       ctx.fillStyle = 'black';
       ctx.fillText(names[i], 240 + 90 * i, 260);
