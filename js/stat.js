6a380c60819582e6beed36e022adb590be6a7072
VarlogMD <demike1987@hotmail.com>
2017-03-29 22:32:38 +0300

    теперь не работает)

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//
 window.renderStatistics = function (ctx, names, times) {
   /*тень*/
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
@@ -18,15 +18,24 @@ window.renderStatistics = function (ctx, names, times) {
   /*Определяем цвет бара*/
   function getRandomColor(playerName) {
     if (playerName !=='Вы') {
-      return 'rgba(0, 0, 255,' + ((randomInteger(2, 10) * 0.1).toFixed(1)) + ')';
+      return 'rgba(0, 0, 255,' + ((randomInteger(2, 10) * 0.1) + ')')
     } else {
       return  'rgba(255, 0, 0, 1)';
     }
-    
+
+
+    /*лимит высоты гистограммы*/
     var max = -1;
     var maxIndex = -1;
-    
-    /*лимит высоты гистограммы*/
+
+    for (var i = 0 ; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+        maxIndex = i;
+      }
+    }
+
     var histogramHeight = 150;
     var step = histogramHeight / max;
 
@@ -34,6 +43,7 @@ window.renderStatistics = function (ctx, names, times) {
     for (var i = 0 ; i < times.length; i++){
       ctx.fillStyle = getRandomColor();
       ctx.fillRect(150, 240, 40, - times[i] * step);
+      ctx.fillStyle = 'black';
       ctx.fillText(names[i], 150 + 90 * i, 260);
       ctx.fillText(times[i].toFixed(0), 150 + 90 * i, 240 - times[i] * step -10);
     }
@@ -46,4 +56,5 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 };
+
 
